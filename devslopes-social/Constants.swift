2196aa8087ac9a24bcba43cc3f37c09760fb09d4
Joe K <joe@joe-me294khaui-MacBook-Pro.local>
2017-03-18 21:56:42 +0900

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by joe_mac on 03/18/2017.
+//  Copyright © 2017 joe_mac. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
